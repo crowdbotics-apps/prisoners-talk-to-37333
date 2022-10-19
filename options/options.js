7253b43db349fd0036d9ad0b7e2144a9013a63d2
Crowdbotics <team@crowdbotics.com>
2022-10-19 02:16:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "prisoners_talk_to_37333",
+  url: "https://prisoners_talk_to_37333.botics.co",
+  api: "https://prisoners_talk_to_37333.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
